c6157b1e7aec5cb45fb3f6f702aae312489af480
LynnrinChan <lynnrin@lynnrin.moe>
2022-10-18 02:23:25 +0000

    nabu: Add Xiaomi Stylus and keyboard support
    Signed-off-by: LynnrinChan <lynnrin@lynnrin.moe>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,20 +20,40 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
     private static final String TAG = "XiaomiParts";
+
+    public static final String SHARED_STYLUS = "shared_stylus";
+    public static final String SHARED_KEYBOARD = "shared_keyboard";
+
+    private ITouchFeature mTouchFeature;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
+        SharedPreferences stylus_prefs = context.getSharedPreferences(SHARED_STYLUS, Context.MODE_PRIVATE);
+        SharedPreferences keyboard_prefs = context.getSharedPreferences(SHARED_KEYBOARD, Context.MODE_PRIVATE);
+        try {
+            mTouchFeature = ITouchFeature.getService();
+            mTouchFeature.setTouchMode(20, stylus_prefs.getInt(SHARED_STYLUS, 0));
+        } catch (Exception e) {
+        }
+        if (keyboard_prefs.getInt(SHARED_KEYBOARD, 1) == 0)
+            FileUtils.writeLine("/sys/devices/platform/soc/soc:xiaomi_keyboard/xiaomi_keyboard_conn_status", "enable_keyboard");
+        else
+            FileUtils.writeLine("/sys/devices/platform/soc/soc:xiaomi_keyboard/xiaomi_keyboard_conn_status", "disable_keyboard");
     }
 }
