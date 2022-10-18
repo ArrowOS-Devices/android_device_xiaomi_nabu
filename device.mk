968a1505748f7a280d74529b80b1510f59e0978a
kubersharma001 <kubersharma001@gmail.com>
2022-10-18 08:48:53 +0000

    nabu: Do not ship modules to vendor_boot

--- device.mk
+++ device.mk
@@ -167,7 +167,3 @@ PRODUCT_PACKAGES_DEBUG += \
 # Vendor boot
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
-
-# Vendor boot modules
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(NABU_PREBUILT)/modules/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules)
