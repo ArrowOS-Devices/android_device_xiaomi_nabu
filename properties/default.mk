3d65e6096881263a3aa77c3ed9a802a965b31b2a
kubersharma001 <kubersharma001@gmail.com>
2022-10-20 08:05:24 +0000

    nabu: default.mk: Move props without condition to product.prop
    - default.mk is only used as adb check, else we dont need to use it

--- properties/default.mk
+++ properties/default.mk
@@ -6,10 +6,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0
 endif
-
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.radio.noril=true
-
-# Use gestures by default
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
